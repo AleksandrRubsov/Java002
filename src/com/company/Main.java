184bd2cd92f1201d8489af34b2b0ffe48aa8e549
util55 <sanya-rubtsov-2015@mail.ru>
2020-09-09 17:27:25 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        Author Author = new Author("Savva","savva@mail.ru",'M');
+        System.out.println(Author);
+
+    }
+}
